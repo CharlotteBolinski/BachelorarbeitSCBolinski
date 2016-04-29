8d4d3b925d8592bf34ff08e0a6efbec07d6546d2
CharlotteBolinski <s0545839@htw-berlin.de>
2016-04-29 14:40:26 +0200

    neue Ebenen-Funktion, RotationZentrum war falsch gespeichert

---
+++ Transformation_frame_export.m
@@ -0,0 +1,6 @@
+function [ output_werte_matrix ] = Transformation_frame_export( input_werte_matrix, xEbene, yEbene, zEbene, bis_punkt_translation, bis_winkel_rotation, achse)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
